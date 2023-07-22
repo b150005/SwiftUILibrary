0627135c7371ba6d8b7b939c8d09c8dc487e912e
b150005 <b150005@outlook.jp>
2023-07-22 10:55:19 +0900

    Initial Commit

---
+++ SwiftUILibrary/SwiftUILibraryApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUILibraryApp.swift
+//  SwiftUILibrary
+//
+//  Created by 伊藤 直輝 on 2023/07/22.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftUILibraryApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
