3faf36aae37d49dda79ff1ea2b02e694620749c9
b150005 <b150005@outlook.jp>
2023-09-01 23:13:16 +0900

    :adhesive_bandage: add a protocol and apply to each view using Section and Group

---
+++ SwiftUILibrary/Views/SectionGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  SectionGroupView.swift
+//  SwiftUILibrary
+//
+//  Created by 伊藤 直輝 on 2023/09/01.
+//
+
+import SwiftUI
+
+struct SectionGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SectionGroupView()
+}
